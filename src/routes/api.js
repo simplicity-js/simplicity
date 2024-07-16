a086cb12d640f6d37c65b0bf2473d4c562e3fc87
simplymichael <simplymichaelorji@gmail.com>
2024-07-16 21:45:35 +0100

    feat: get server info
    implement functionality for getting the server info and a route to get the server's health status

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,7 @@
 const { STATUS_CODES, STATUS_TEXTS } = require(
   "@simplicityjs/framework/component/http");
-
 const Router = require("@simplicityjs/framework/component/router");
+const serverStatus = require("../app/http/middleware/server-status");
 
 const router = Router.router();
 
@@ -11,5 +11,22 @@ router.get("/", (req, res) => res.status(STATUS_CODES.HTTP_OK).json({
   message: STATUS_TEXTS[STATUS_CODES.HTTP_OK],
 }));
 
+router.get({ uri: "/server-status", middleware: serverStatus }, (req, res) => {
+  const config = req.app.resolve("config");
+  const { application, server } = res.serverState;
+  const { status, uptime, utilization } = server;
+
+  return res.json({
+    application: {
+      ...application,
+      name: config.get("app.name"),
+    },
+    server: {
+      status,
+      uptime,
+      utilization,
+    },
+  });
+});
 
 module.exports = router;
