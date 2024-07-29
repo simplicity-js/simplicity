e781bfa4afd44c6ce8070a9dbe7e4752761e41c1
simplymichael <simplymichaelorji@gmail.com>
2024-07-29 13:38:45 +0100

    refactor: create mongoose models loader; create parent controller class

---
+++ src/app/http/models/mongoose/index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const fs = require("node:fs");
+const path = require("node:path");
+const mongoose = require("mongoose");
+const Connections = require("@simplicityjs/framework/connections");
+const config = require("../../../../config");
+
+const db = {};
+const basename = path.basename(__filename);
+const connectionName = config.get("database.mongoose");
+const connectionConfig = config.get("database.connections")[connectionName];
+const connection = Connections.get(connectionName, connectionConfig);
+
+function createModel(modelName, schema, connection) {
+  if(mongoose.models[modelName]) {
+    return mongoose.models[modelName];
+  }
+
+  if(connection?.models[modelName]) {
+    return connection.models[modelName];
+  }
+
+  return (connection
+    ? connection.model(modelName, schema)
+    : mongoose.model(modelName, schema)
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter(f => f.indexOf(".") !== 0 && f !== basename && f.slice(-3) === ".js")
+  .forEach(file => {
+    const modelClass = require(path.join(__dirname, file));
+    const modelName = modelClass.name;
+    const modelSchema = ""; 
+    const model = createModel(modelName, modelSchema, connection);
+
+    db[modelClass.name] = model;
+
+    model.sync();
+  });
+
+module.exports = db;
