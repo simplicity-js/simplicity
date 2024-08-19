71ac81b49cd3c67cc70504402c8701271d9b998b
simplymichael <simplymichaelorji@gmail.com>
2024-08-19 09:54:29 +0100

    refactor: :hammer: refactor the start script (bin/start-server.js)

--- bin/start-server.js
+++ bin/start-server.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+const path = require("node:path");
 const { parseArgs } = require("node:util");
 const exec = require("./exec");
 
+const rootDir = path.dirname(__dirname);
 const options = parseArgs({
   allowPositionals: true,
   options: {
@@ -10,16 +12,15 @@ const options = parseArgs({
   },
 });
 
-startServer();
-
-
 function startServer() {
-  let command = "node ./src/index.js";
+  let command = `node ${rootDir}${path.sep}src${path.sep}index.js`;
   const port = options.values.port;
 
   if(port) {
     command += ` --port ${port}`;
   }
 
-  exec(command).catch(() => {});
+  exec(command).catch((err) => console.error(err));
 }
+
+startServer();
