9371aedeafe73bef2f74f244a02c2a2aa7602b8b
simplymichael <simplymichaelorji@gmail.com>
2024-09-14 21:34:10 +0100

    feat(csrf): :sparkles: Add support for framework's CSRF protection feature

---
+++ src/config/csrf.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+/**
+ * -----------------------------------------------------------
+ * Cross-Site Request Forgery (CSRF) protection configuration
+ * -----------------------------------------------------------
+ */
+module.exports = {
+  /*
+   * The list of web-based routes to exclude from CSRF protection
+   */
+  exclude: [],
+};
